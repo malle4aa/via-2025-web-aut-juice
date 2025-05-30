68150b2c2cea90563b6892a75aed048d67a70c63
malle4aa <malichbogdan11@gmail.com>
2025-05-30 20:30:37 +0300

    Update OrderCompletionPage.js

--- cypress/pageObjects/OrderCompletionPage.js
+++ cypress/pageObjects/OrderCompletionPage.js
@@ -1,6 +1,5 @@
 export class OrderCompletionPage {
   static verifyConfirmation() {
-    cy.contains("Thank you for your purchase!").should("exist");
+    cy.contains("Thank you for your purchase!").should("be.visible");
   }
 }
-
