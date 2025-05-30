1692ac90c7b1dcafeab9285e7cb79cce4ba9a91b
malle4aa <malichbogdan11@gmail.com>
2025-05-30 20:29:23 +0300

    Update BasketPage.js

--- cypress/pageObjects/BasketPage.js
+++ cypress/pageObjects/BasketPage.js
@@ -1,4 +1,8 @@
 export class BasketPage {
+  static goToBasket() {
+    cy.get('[aria-label="Show the shopping cart"]').click();
+  }
+
   static checkout() {
     cy.contains("Checkout").click();
   }
