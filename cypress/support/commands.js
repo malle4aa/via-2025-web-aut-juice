6b3a787c5d04456d1360f93ae2f65d1d9cfa5ec8
malle4aa <malichbogdan11@gmail.com>
2025-05-30 20:59:03 +0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,9 @@ Cypress.Commands.add("login", (username, password) => {
     }
   );
 });
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("/#/login");
+  cy.get("#email").type(email);
+  cy.get("#password").type(password);
+  cy.get("#loginButton").click();
+});
