1b3aedf614fae3df9e77655c73690c95c7f7a3b0
malle4aa <malichbogdan11@gmail.com>
2025-05-30 20:56:06 +0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,26 @@
 Cypress.Commands.add("login", (username, password) => {
-  cy.session([username, password], () => {
-    cy.request({
-      method: "POST",
-      url: "/rest/user/login",
-      body: { email: username, password: password },
-    }).then((response) => {
-      window.localStorage.setItem("token", response.body.authentication.token);
-      window.sessionStorage.setItem("bid", response.body.authentication.bid);
-      cy.setCookie("token", response.body.authentication.token);
-      cy.setCookie("cookieconsent_status", "dismiss");
-      cy.setCookie("welcomebanner_status", "dismiss");
-    });
-  }, {
-    validate() {
-      cy.request("/whoami").its("status").should("eq", 200);
+  cy.session(
+    [username, password],
+    () => {
+      cy.request({
+        method: "POST",
+        url: "/rest/user/login",
+        body: { email: username, password: password },
+      }).then((response) => {
+        window.localStorage.setItem(
+          "token",
+          response.body.authentication.token
+        );
+        window.sessionStorage.setItem("bid", response.body.authentication.bid);
+        cy.setCookie("token", response.body.authentication.token);
+        cy.setCookie("cookieconsent_status", "dismiss");
+        cy.setCookie("welcomebanner_status", "dismiss");
+      });
     },
-  });
-});
-
-Cypress.Commands.add("register", (email, password) => {
-  cy.get("#emailControl").type(email);
-  cy.get("#passwordControl").type(password);
-  cy.get("#repeatPasswordControl").type(password);
-  cy.get(".mat-select-placeholder").click();
-  cy.contains("Name of your favorite pet?").click();
-  cy.get("#securityAnswerControl").type("Fluffy");
-  cy.get("#registerButton").click();
+    {
+      validate() {
+        cy.request("/whoami").its("status").should("eq", 200);
+      },
+    }
+  );
 });
