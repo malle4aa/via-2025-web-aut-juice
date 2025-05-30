2f10629ff12941c5641e53967ce59c78c2540638
malle4aa <malichbogdan11@gmail.com>
2025-05-30 19:43:49 +0300

    Update juice-shop.cy.js

--- cypress/e2e/juice-shop.cy.js
+++ cypress/e2e/juice-shop.cy.js
@@ -5,6 +5,10 @@ import { DeliveryMethodPage } from "../pageObjects/DeliveryMethodPage";
 import { PaymentOptionsPage } from "../pageObjects/PaymentOptionsPage";
 import { OrderSummaryPage } from "../pageObjects/OrderSummaryPage";
 import { OrderCompletionPage } from "../pageObjects/OrderCompletionPage";
+import { SavedAddressesPage } from "../pageObjects/SavedAddressesPage";
+import { CreateAddressPage } from "../pageObjects/CreateAddressPage";
+import { SavedPaymentMethodsPage } from "../pageObjects/SavedPaymentMethodsPage";
+
 
 describe("Juice-shop scenarios", () => {
   context("Without auto login", () => {
@@ -15,11 +19,7 @@ describe("Juice-shop scenarios", () => {
     });
 
     it("Login", () => {
-      cy.get("#navbarAccount").click();
-      cy.get("#navbarLoginButton").click();
-      cy.get("#email").type("demo");
-      cy.get("#password").type("demo");
-      cy.get("#loginButton").click();
+      cy.login("demo", "demo");
       cy.get("#navbarAccount").click();
       cy.get(".mat-menu-content").should("contain", "demo");
     });
@@ -29,23 +29,9 @@ describe("Juice-shop scenarios", () => {
       const email = `email_${rand}@ebox.com`;
       const password = "Test1234";
 
-      cy.get("#navbarAccount").click();
-      cy.get("#navbarLoginButton").click();
-      cy.contains("Not yet a customer?").click();
-
-      cy.get("#emailControl").type(email);
-      cy.get("#passwordControl").type(password);
-      cy.get("#repeatPasswordControl").type(password);
-
-      cy.get("[aria-label='Open security question']").click();
-      cy.contains("Name of your favorite pet?").click();
-
-      cy.get("#securityAnswerControl").type("Fluffy");
-      cy.get("#registerButton").click();
-
-      cy.get("#email").type(email);
-      cy.get("#password").type(password);
-      cy.get("#loginButton").click();
+      HomePage.goToRegister();
+      cy.register(email, password);
+      cy.login(email, password);
 
       cy.get("#navbarAccount").click();
       cy.get(".mat-menu-content").should("contain", email);
@@ -65,7 +51,7 @@ describe("Juice-shop scenarios", () => {
       cy.get(".mat-dialog-content").should("contain", "Sour but full of vitamins.");
     });
 
-    it("Search 500ml and validate Lemon (multiple cards)", () => {
+    it("Search 500ml and validate Lemon", () => {
       cy.get(".mat-search_icon-search").click();
       cy.get("#searchQuery").type("500ml{enter}");
       cy.contains("Lemon Juice (500ml)").click();
@@ -77,11 +63,11 @@ describe("Juice-shop scenarios", () => {
       cy.get("#searchQuery").type("500ml{enter}");
       cy.contains("Eggfruit Juice (500ml)").click();
       cy.get(".mat-dialog-content").should("contain", "Now with even more exotic flavour.");
-      cy.get(".mat-dialog-close").click();
+      cy.get(".mat-dialog-container button[aria-label='Close Dialog']").click();
 
       cy.contains("Lemon Juice (500ml)").click();
       cy.get(".mat-dialog-content").should("contain", "Sour but full of vitamins.");
-      cy.get(".mat-dialog-close").click();
+      cy.get(".mat-dialog-container button[aria-label='Close Dialog']").click();
 
       cy.contains("Strawberry Juice (500ml)").click();
       cy.get(".mat-dialog-content").should("contain", "Sweet & tasty!");
@@ -90,31 +76,29 @@ describe("Juice-shop scenarios", () => {
     it("Read a review", () => {
       cy.get(".mat-search_icon-search").click();
       cy.get("#searchQuery").type("King{enter}");
-      cy.contains('OWASP Juice Shop "King of the Hill" Facemask').click();
-      cy.get("[aria-label='Show Reviews']").click();
-      cy.get(".mat-dialog-content").should("contain", "K33p5 y0ur ju1cy 5plu773r 70 y0ur53lf!");
+      cy.contains("OWASP Juice Shop \"King of the Hill\" Facemask").click();
+      cy.get("[aria-label='Expand for Reviews']").click();
+      cy.get(".review-text").should("contain", "K33p5 y0ur ju1cy 5plu773r 70 y0ur53lf!");
     });
 
     it("Add a review", () => {
       cy.get(".mat-search_icon-search").click();
       cy.get("#searchQuery").type("Raspberry{enter}");
       cy.contains("Raspberry Juice (1000ml)").click();
-      cy.get("#mat-input-5").type("Tastes like metal");
+      cy.get("textarea[aria-label='Text field to review a product']").type("Tastes like metal");
       cy.contains("Submit").click();
-      cy.get("[aria-label='Show Reviews']").click();
-      cy.get(".mat-dialog-content").should("contain", "Tastes like metal");
+      cy.get("[aria-label='Expand for Reviews']").click();
+      cy.get(".review-text").should("contain", "Tastes like metal");
     });
 
     it("Validate product card amount", () => {
-      cy.get(".item-card").should("have.length", 12);
-
+      cy.get(".mat-card").should("have.length", 12);
       cy.get("mat-select[aria-label='Items per page']").click();
-      cy.contains("24").click();
-      cy.get(".item-card").should("have.length", 24);
-
+      cy.get("mat-option").contains("24").click();
+      cy.get(".mat-card").should("have.length", 24);
       cy.get("mat-select[aria-label='Items per page']").click();
-      cy.contains("36").click();
-      cy.get(".item-card").should("have.length", 35);
+      cy.get("mat-option").contains("36").click();
+      cy.get(".mat-card").should("have.length", 35);
     });
 
     it("Buy Girlie T-shirt", () => {
@@ -122,7 +106,6 @@ describe("Juice-shop scenarios", () => {
       cy.get("#searchQuery").type("Girlie{enter}");
       cy.contains("Add to Basket").click();
       cy.get('[aria-label="Show the shopping cart"]').click();
-
       BasketPage.checkout();
       SelectAddressPage.selectFakedom();
       DeliveryMethodPage.selectStandard();
@@ -135,29 +118,24 @@ describe("Juice-shop scenarios", () => {
       cy.get("#navbarAccount").click();
       cy.contains("Orders & Payment").click();
       cy.contains("My saved addresses").click();
-
-      BasketPage.clickAddNewAddress();
-      SelectAddressPage.fillAddressForm({
-        name: "John Doe",
-        phone: "1234567890",
-        country: "United Fakedom",
-        city: "Fake City",
-        zip: "12345",
-        address: "123 Fake St"
-      });
-      BasketPage.submitAddress();
-
-      cy.contains("United Fakedom").should("be.visible");
+      SavedAddressesPage.clickAddAddress();
+      CreateAddressPage.fillAddress();
+      CreateAddressPage.submit();
+      SavedAddressesPage.verifyNewAddress();
     });
 
     it("Add payment option", () => {
       cy.get("#navbarAccount").click();
       cy.contains("Orders & Payment").click();
       cy.contains("My payment options").click();
+      SavedPaymentMethodsPage.clickAddCard();
+      SavedPaymentMethodsPage.fillCardInfo("John Doe", "1234123412345678", 7, 2090);
+      SavedPaymentMethodsPage.submit();
+      SavedPaymentMethodsPage.verifyCard("5678");
+    });
+  });
+});
 
-      PaymentOptionsPage.clickAddNewCard();
-      PaymentOptionsPage.fillCardForm({
-        name: "John Doe",
         number: "4111111111111111",
         expiryMonth: "7",
         expiryYear: "2090"
