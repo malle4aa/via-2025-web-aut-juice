4005bb5a32643066ceb25fc14b0e41d1abd8a406
malle4aa <malichbogdan11@gmail.com>
2025-05-30 20:28:15 +0300

    Update juice-shop.cy.js

--- cypress/e2e/juice-shop.cy.js
+++ cypress/e2e/juice-shop.cy.js
@@ -1,54 +1,36 @@
-// cypress/e2e/juice-shop.cy.js
 import { HomePage } from "../pageObjects/HomePage";
+import { LoginPage } from "../pageObjects/LoginPage";
 import { BasketPage } from "../pageObjects/BasketPage";
 import { SelectAddressPage } from "../pageObjects/SelectAddressPage";
 import { DeliveryMethodPage } from "../pageObjects/DeliveryMethodPage";
-import { PaymentOptionsPage } from "../pageObjects/PaymentOptionPage";
+import { PaymentOptionPage } from "../pageObjects/PaymentOptionPage";
 import { OrderSummaryPage } from "../pageObjects/OrderSummaryPage";
 import { OrderCompletionPage } from "../pageObjects/OrderCompletionPage";
-import { LoginPage } from "../pageObjects/LoginPage";
 
-
-// A test to validate each page object is functioning
-// This test flow logs in, buys a product, and completes checkout, touching all 9 page objects
-
-describe("Juice Shop Full Flow Test - All Page Objects", () => {
-  beforeEach(() => {
-    cy.login("demo", "demo");
-    HomePage.visit();
-    cy.get(".mat-search_icon-search").click();
-    cy.get("#searchQuery").type("Girlie{enter}");
-    cy.contains("Add to Basket").click();
-    cy.get('[aria-label="Show the shopping cart"]').click();
+describe("Juice Shop Full Flow Test", () => {
+  before(() => {
+    cy.visit("/");
+    HomePage.dismissWelcomeBanner();
+    HomePage.dismissCookieBanner();
   });
 
-  it("Runs through all page objects", () => {
-    // Use BasketPage
+  it("Login using page object", () => {
+    HomePage.openLogin();
+    LoginPage.login("demo", "demo");
+    HomePage.verifyUserLoggedIn("demo");
+  });
+
+  it("Add product to basket and checkout", () => {
+    HomePage.searchProduct("Girlie");
+    HomePage.addToBasket("Girlie");
+
+    BasketPage.goToBasket();
     BasketPage.checkout();
 
-    // Use SelectAddressPage
     SelectAddressPage.selectFakedom();
-
-    // Use DeliveryMethodPage
     DeliveryMethodPage.selectStandard();
-
-    // Use PaymentOptionsPage
-    PaymentOptionsPage.selectCardEndingWith("5678");
-
-    // Use OrderSummaryPage
+    PaymentOptionPage.selectCardEndingWith("5678");
     OrderSummaryPage.placeOrder();
-
-    // Use OrderCompletionPage
     OrderCompletionPage.verifyConfirmation();
-
-    // Reuse HomePage
-    HomePage.visit();
-    cy.get(".mat-toolbar").should("contain", "OWASP Juice Shop");
-
-    // Use LoginPage for log out
-    cy.get("#navbarAccount").click();
-    cy.contains("Logout").click();
-    LoginPage.visit();
-    LoginPage.emailField.should("be.visible");
   });
 });
