7b82cc0bfb31a02881e874aa92aa3caac191075f
malle4aa <malichbogdan11@gmail.com>
2025-05-30 19:34:36 +0300

    Update PaymentOptionPage.js

--- cypress/pageObjects/PaymentOptionPage.js
+++ cypress/pageObjects/PaymentOptionPage.js
@@ -1,8 +1,6 @@
 export class PaymentOptionsPage {
   static selectCardEndingWith(digits) {
-    cy.get(".payment-method-card")
-      .contains(digits)
-      .click();
+    cy.contains(new RegExp(digits + "$")).click();
     cy.contains("Continue").click();
   }
 }
