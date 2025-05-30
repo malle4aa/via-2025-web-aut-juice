994a5ec2aada37df261a16ad0545102d2c158556
malle4aa <malichbogdan11@gmail.com>
2025-05-31 02:16:34 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here (optional)
     },
     viewportWidth: 1440,
     viewportHeight: 900,
     defaultCommandTimeout: 5000,
     baseUrl: "http://127.0.0.1:3000",
-    specPattern: "cypress/e2e/**/*.cy.js",         // ✅ Ensure Cypress looks for tests
-    supportFile: "cypress/support/e2e.js",          // ✅ Ensures custom commands are loaded
+    specPattern: "cypress/e2e/**/*.cy.js",         
+    supportFile: "cypress/support/e2e.js",          
   },
 });
