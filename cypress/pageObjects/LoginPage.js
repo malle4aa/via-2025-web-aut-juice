b11562c379ec7d190f5437fa6d631fad59b34875
malle4aa <malichbogdan11@gmail.com>
2025-05-30 19:12:50 +0300

    Update LoginPage.js

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -1,11 +1,23 @@
-import { BasePage } from "../pageObjects/basePage";
+import { BasePage } from "./basePage";
 
 export class LoginPage extends BasePage {
   static get url() {
     return "/#/login";
   }
 
-  static get elementName() {
-    return cy.get("elementSelector");
+  static get emailField() {
+    return cy.get("#email");
+  }
+
+  static get passwordField() {
+    return cy.get("#password");
+  }
+
+  static get loginButton() {
+    return cy.get("#loginButton");
+  }
+
+  static get newCustomerLink() {
+    return cy.contains("Not yet a customer?");
   }
 }
