9fbfc4be1b9bfebb640f0c87820c40a52456e402
malle4aa <malichbogdan11@gmail.com>
2025-05-30 20:46:06 +0300

    Update juice-shop.cy.js

--- cypress/e2e/juice-shop.cy.js
+++ cypress/e2e/juice-shop.cy.js
@@ -1,93 +1,166 @@
+// cypress/e2e/juice-shop.cy.js
 import { HomePage } from "../pageObjects/HomePage";
-import { LoginPage } from "../pageObjects/LoginPage";
 import { BasketPage } from "../pageObjects/BasketPage";
 import { SelectAddressPage } from "../pageObjects/SelectAddressPage";
 import { DeliveryMethodPage } from "../pageObjects/DeliveryMethodPage";
-import { PaymentOptionPage } from "../pageObjects/PaymentOptionPage";
+import { PaymentOptionsPage } from "../pageObjects/PaymentOptionsPage";
 import { OrderSummaryPage } from "../pageObjects/OrderSummaryPage";
 import { OrderCompletionPage } from "../pageObjects/OrderCompletionPage";
+import { SavedAddressesPage } from "../pageObjects/SavedAddressesPage";
+import { CreateAddressPage } from "../pageObjects/CreateAddressPage";
+import { SavedPaymentMethodsPage } from "../pageObjects/SavedPaymentMethodsPage";
 
-describe("Juice Shop - Full Test Suite", () => {
-  before(() => {
-    cy.session("login", () => {
-      cy.login("demo", "demo");
+function generateEmail() {
+  const rand = Math.floor(Math.random() * 100000);
+  return `email_${rand}@ebox.com`;
+}
+
+describe("Juice-shop scenarios", () => {
+  context("Without auto login", () => {
+    beforeEach(() => {
+      HomePage.visit();
+      HomePage.dismissButton.click();
+      HomePage.meWantItButton.click();
+    });
+
+    it("Login", () => {
+      cy.get("button[aria-label='Show/hide account menu']").click();
+      cy.get("button[aria-label='Show Login dialog']").click();
+      cy.get("input#email").type("demo");
+      cy.get("input#password").type("demo");
+      cy.get("button#loginButton").click();
+      cy.get("button[aria-label='Show/hide account menu']").click();
+      cy.contains("demo").should("exist");
+    });
+
+    it("Registration", () => {
+      const email = generateEmail();
+      const password = "Test1234";
+      cy.get("button[aria-label='Show/hide account menu']").click();
+      cy.get("button[aria-label='Show Login dialog']").click();n      cy.contains("Not yet a customer?").click();
+      cy.get("input#emailControl").type(email);
+      cy.get("input#passwordControl").type(password);
+      cy.get("input#repeatPasswordControl").type(password);
+      cy.get("mat-select[name='securityQuestion']").click();
+      cy.contains("Name of your favorite pet?").click();
+      cy.get("input#securityAnswerControl").type("Fluffy");
+      cy.get("button#registerButton").click();
+      cy.get("input#email").type(email);
+      cy.get("input#password").type(password);
+      cy.get("button#loginButton").click();
+      cy.get("button[aria-label='Show/hide account menu']").click();
+      cy.contains(email).should("exist");
     });
   });
 
-  beforeEach(() => {
-    cy.visit("/");
-    HomePage.dismissWelcomeBanner();
-    HomePage.dismissCookieBanner();
-  });
+  context("With auto login", () => {
+    beforeEach(() => {
+      cy.login("demo", "demo");
+      HomePage.visit();
+    });
 
-  it("Verify login from UI", () => {
-    HomePage.openLogin();
-    LoginPage.login("demo", "demo");
-    HomePage.verifyUserLoggedIn("demo");
-  });
+    it("Search and validate Lemon", () => {
+      cy.get("mat-icon[aria-label='Open search dialog']").click();
+      cy.get("input#searchQuery").type("Lemon{enter}");
+      cy.contains("Lemon Juice (500ml)").click();
+      cy.contains("Sour but full of vitamins.").should("exist");
+    });
 
-  it("Search and view Lemon Juice", () => {
-    HomePage.searchProduct("Lemon");
-    cy.contains("Lemon Juice (500ml)").click();
-    cy.get(".mat-dialog-content").should("contain", "Sour but full of vitamins.");
-  });
+    it("Search 500ml and validate Lemon", () => {
+      cy.get("mat-icon[aria-label='Open search dialog']").click();
+      cy.get("input#searchQuery").type("500ml{enter}");
+      cy.contains("Lemon Juice (500ml)").click();
+      cy.contains("Sour but full of vitamins.").should("exist");
+    });
 
-  it("Add review for product", () => {
-    HomePage.searchProduct("Raspberry");
-    cy.contains("Raspberry Juice (1000ml)").click();
-    cy.get("#comment").type("Tastes like metal");
-    cy.contains("Submit").click();
-    cy.get(".mat-expansion-panel-header").click();
-    cy.get(".review-text").should("contain", "Tastes like metal");
-  });
+    it("Search 500ml and validate cards", () => {
+      cy.get("mat-icon[aria-label='Open search dialog']").click();
+      cy.get("input#searchQuery").type("500ml{enter}");
+      cy.contains("Eggfruit Juice (500ml)").click();
+      cy.contains("Now with even more exotic flavour.").should("exist");
+      cy.get("button[aria-label='Close Dialog']").click();
+      cy.contains("Lemon Juice (500ml)").click();
+      cy.contains("Sour but full of vitamins.").should("exist");
+      cy.get("button[aria-label='Close Dialog']").click();
+      cy.contains("Strawberry Juice (500ml)").click();
+      cy.contains("Sweet & tasty!").should("exist");
+    });
 
-  it("Validate pagination product card count", () => {
-    cy.get(".mat-grid-tile").should("have.length", 12);
-    cy.get("mat-select[aria-label='Items per page']").click();
-    cy.get("mat-option").contains("24").click();
-    cy.get(".mat-grid-tile").should("have.length", 24);
-    cy.get("mat-select[aria-label='Items per page']").click();
-    cy.get("mat-option").contains("36").click();
-    cy.get(".mat-grid-tile").should("have.length", 35); // Expected amount for 36 may differ
-  });
+    it("Read a review", () => {
+      cy.get("mat-icon[aria-label='Open search dialog']").click();
+      cy.get("input#searchQuery").type("King{enter}");
+      cy.contains("King of the Hill").click();
+      cy.get("mat-expansion-panel").click();
+      cy.contains("K33p5 y0ur ju1cy 5plu773r 70 y0ur53lf!").should("exist");
+    });
 
-  it("Place full order for Girlie T-shirt", () => {
-    HomePage.searchProduct("Girlie");
-    HomePage.addToBasket("Girlie");
-    BasketPage.goToBasket();
-    BasketPage.checkout();
-    SelectAddressPage.selectFakedom();
-    DeliveryMethodPage.selectStandard();
-    PaymentOptionPage.selectCardEndingWith("5678");
-    OrderSummaryPage.placeOrder();
-    OrderCompletionPage.verifyConfirmation();
-  });
+    it("Add a review", () => {
+      cy.get("mat-icon[aria-label='Open search dialog']").click();
+      cy.get("input#searchQuery").type("Raspberry{enter}");
+      cy.contains("Raspberry Juice (1000ml)").click();
+      cy.get("textarea[placeholder='What did you think?']").type("Tastes like metal");
+      cy.get("button#submitButton").click();
+      cy.get("mat-expansion-panel").click();
+      cy.contains("Tastes like metal").should("exist");
+    });
 
-  it("Add new address", () => {
-    cy.get("#navbarAccount").click();
-    cy.contains("Orders & Payment").click();
-    cy.contains("My saved addresses").click();
-    cy.contains("Add New Address").click();
-    cy.get("#mat-input-1").type("Test");
-    cy.get("#mat-input-2").type("User");
-    cy.get("#mat-input-3").type("Street 1");
-    cy.get("#mat-input-4").type("12345");
-    cy.get("#mat-input-5").type("City");
-    cy.get("#mat-input-6").type("Country");
-    cy.get("button#submitButton").click();
-    cy.get(".mat-cell").should("contain", "Street 1");
-  });
+    it("Validate product card amount", () => {
+      cy.get("mat-card").should("have.length", 12);
+      cy.get("mat-select[aria-label='Items per page']").click();
+      cy.contains("24").click();
+      cy.get("mat-card").should("have.length", 24);
+      cy.get("mat-select[aria-label='Items per page']").click();
+      cy.contains("36").click();
+      cy.get("mat-card").should("have.length", 35);
+    });
 
-  it("Add new payment method", () => {
-    cy.get("#navbarAccount").click();
-    cy.contains("Orders & Payment").click();
-    cy.contains("My payment options").click();
-    cy.contains("Add new card").click();
-    cy.get("#mat-input-1").type("Test User");
-    cy.get("#mat-input-2").type("1234567812345678");
-    cy.get("#mat-input-3").type("7");
-    cy.get("#mat-input-4").type("2090");
-    cy.get("#submitButton").click();
-    cy.get(".mat-cell").should("contain", "****5678");
+    it("Buy Girlie T-shirt", () => {
+      cy.get("mat-icon[aria-label='Open search dialog']").click();
+      cy.get("input#searchQuery").type("Girlie{enter}");
+      cy.contains("Add to Basket").click();
+      cy.get("button[routerlink='/basket']").click();
+      BasketPage.checkoutButton().click();
+      SelectAddressPage.selectAddress("United Fakedom");
+      SelectAddressPage.continueButton().click();
+      DeliveryMethodPage.selectStandardDelivery();
+      DeliveryMethodPage.continueButton().click();
+      PaymentOptionsPage.selectCard("5678");
+      PaymentOptionsPage.continueButton().click();
+      OrderSummaryPage.placeOrderButton().click();
+      OrderCompletionPage.confirmation().should("contain", "Thank you for your purchase!");
+    });
+
+    it("Add address", () => {
+      cy.get("button[aria-label='Show/hide account menu']").click();
+      cy.contains("Orders & Payment").click();
+      cy.contains("My saved addresses").click();
+      SavedAddressesPage.addAddressButton().click();
+      CreateAddressPage.fillAddress({
+        country: "Latvia",
+        name: "Test User",
+        mobile: "12345678",
+        zip: "LV-1000",
+        address: "Test Street 1",
+        city: "Riga",
+      });
+      CreateAddressPage.submitButton().click();
+      cy.contains("Test Street 1").should("exist");
+    });
+
+    it("Add payment option", () => {
+      cy.get("button[aria-label='Show/hide account menu']").click();
+      cy.contains("Orders & Payment").click();
+      cy.contains("My payment options").click();
+      SavedPaymentMethodsPage.addCardButton().click();
+      SavedPaymentMethodsPage.fillCardDetails({
+        name: "Test User",
+        number: "1234123412345678",
+        month: "7",
+        year: "2090",
+      });
+      SavedPaymentMethodsPage.submitButton().click();
+      cy.contains("**** **** **** 5678").should("exist");
+    });
   });
 });
+
